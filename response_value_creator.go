aaee7dccfd10dcd98b436f5a84fe7085402842ad
Samuel Nelson <valczir.darkvein@gmail.com>
2014-12-29 09:36:38 -0700

    Update docs for ResponseElementConverter

--- response_value_creator.go
+++ response_value_creator.go
@@ -7,17 +7,14 @@ import (
 // ResponseElementConverter is a type that converts itself to a
 // different structure or type when it is used as a sub-element of a
 // response.  It is not used when the type is the top-level response
-// data.
+// data, nor when the top-level response data is a list and the type
+// is included in that list.
 //
 // This is particularly useful for compressing a large struct to
-// something much smaller in two situations:
-//
-// 1. You are responding with a list of *many* of the large struct
-// type, often as part of a `GET /resource` response.
-//
-// 2. You are responding with a struct type that includes the large
-// struct type as a field, often as part of a `GET /otherresource/id`
-// response.
+// something much smaller when it is a field in another struct, or
+// for only returning links to elements when the elements aren't
+// fully populated and another database query isn't necessarily
+// needed.
 type ResponseElementConverter interface {
 	// ResponseElementData should return the data that will be used
 	// instead of the ResponseElementConverter *only* when it is a
