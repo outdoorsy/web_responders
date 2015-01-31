ce87ef169fb5d311122967a03a1a28299219cef8
Samuel Nelson <valczir.darkvein@gmail.com>
2015-01-30 20:48:19 -0700

    Added json as a fallback tag

--- tag_finder.go
+++ tag_finder.go
@@ -10,7 +10,7 @@ func ResponseTag(field reflect.StructField) string {
 	if name = field.Tag.Get("response"); name != "" {
 		return name
 	}
-	if name = field.Tag.Get("db"); name != "" && name != "-" {
+	if name = field.Tag.Get("json"); name != "" && name != "-" {
 		return name
 	}
 	return strings.ToLower(field.Name)
